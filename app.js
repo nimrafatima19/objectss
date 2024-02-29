8f3e48d4ceb72dd3d9a82707dfbed9c157fdf55e
nimrafatima19 <145598666+nimrafatima19@users.noreply.github.com>
2024-02-29 12:42:55 +0500

    img change

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var purseList ={
          },
         calches:{
             name:"Calche",
-            price:"PKR 8000",
+            price:"PKR 8,000",
             image:"images/gucci2.png"
         }
 
@@ -15,26 +15,57 @@ var purseList ={
     Prada:{
         bagpacks:{
             name:"Bag Pack",
-            price:"PKR 5000",
-            image:"images/prada2.png"
+            price:"PKR 5,000",
+            image:"images/prada4.png"
 
         },
         shoulderbags:{
 
             name:"Shoulder Bag",
-            price:"PKR 6000",
-            image:"images/prada1.png"
+            price:"PKR 6,000",
+            image:"images/prada3.png"
         }
 
     },
    
     Chanel:{
-
-    },
+      shoulderbags:{
+        name:"Shoulder Bag",
+        price:"PKR 5,000",
+        image:"images/chanel1.png"
+      },
+      bagpacks:{
+        name:"Bag Pack",
+        price:"PKR 8,000",
+        image:"images/chanel2.png"
+      }
+},
     Fendi:{
+        shoulderbags:{
+            name:"Shoulder Bags",
+            price:"PKR 1,0000",
+            image:"images/fendi1.png"
+        },
+        bagpacks:{
+            name:"Bag Pack",
+            price:"PKR 12,000",
+            image:"images/fendi2.png"
+
+
+        }
 
     },
     Aldo:{
+        shoulderbags:{
+            name:"Shoulder Bag",
+            price:"PKR 30,000",
+            image:"images/aldo2.png"
+        },
+        bagpacks:{
+            name:"Bag Pack",
+            price:"PKR 15,000",
+            image:"images/aldo1.png"
+        }
 
     }
 }
