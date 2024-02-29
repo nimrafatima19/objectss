d3f3640be61e0090fa572eabeb3cfea01359d272
nimrafatima19 <145598666+nimrafatima19@users.noreply.github.com>
2024-02-29 10:38:49 +0500

    imgs

--- app.js
+++ app.js
@@ -3,27 +3,31 @@ var purseList ={
         Hand_Bag:{
             name:"Hand Bag",
             price:"PKR 10,000",
-            image:"images/image-removebg-preview (20).png"
+            image:"images/gucci1.png"
          },
         calches:{
             name:"Calche",
             price:"PKR 8000",
-            image:"images/bag-brown.jpg"
-        }
-
-    },
-    Parada:{
-        bagpacks:{
-
-        },
-        shoulderbags:{
-
+            image:"images/gucci2.png"
         }
 
     },
     Prada:{
+        bagpacks:{
+            name:"Bag Pack",
+            price:"PKR 5000",
+            image:"images/prada2.png"
+
+        },
+        shoulderbags:{
+
+            name:"Shoulder Bag",
+            price:"PKR 6000",
+            image:"images/prada1.png"
+        }
 
     },
+   
     Chanel:{
 
     },
@@ -39,11 +43,19 @@ for(var key in purseList){
     for(var key1 in purseList[key]){
         var obj = purseList[key][key1];
         main.innerHTML +=`
-<div class="card1">
-<div class="cardImg"><img src="${obj.image}"></div>
-<div class="card_price">${obj.name} </div>
-<div class="card_type">${obj.price}</div>
-</div>
-`
+        <div class="card1">
+ <div class="cardImg"><img src="${obj.image}"></div>
+ <div class="card_price">${obj.name} </div>
+ <div class="card_type">${obj.price}</div>
+ </div>
+        `
     }
 }
+
+
+
+
+
+
+
+
